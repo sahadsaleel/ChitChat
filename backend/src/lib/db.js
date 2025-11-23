e3671790fda87c7bb54b39c1a6741fe08b0e1dc4
Sahad Saleel <saleelsahad@gmail.com>
2025-11-23 20:18:22 +0530

    Fix: updated MongoDB env variable

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
     try {
-        const conn = await mongoose.connect(process.env.mongoDB_URI)
+        const conn = await mongoose.connect(process.env.MONGODB_URI)
         console.log(`MongoDB connected : ${conn.connection.host}`)
     } catch (error) {
         console.log("MOngoDB connection error :" , error)
